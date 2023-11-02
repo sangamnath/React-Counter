bb5f901e83e3c01a76c061041f2dacfe9df2f2bf
Sangam Teli <steli@mdowinsurance.com>
2023-11-02 10:56:28 -0500

    counter 2.0

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react"
+import './style.css';
 
 function App() {
+
+const [count, setCount] = React.useState(0)
+
+function add(){
+  setCount(prevCount => prevCount + 1);
+}
+function subtract(){
+  setCount(prevCount => prevCount - 1);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+     </div>
   );
 }
 
