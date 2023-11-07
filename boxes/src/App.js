3676f739c11b9f06b3ac3583e1f1e64e773ad26e
Sangam Teli <steli@mdowinsurance.com>
2023-11-07 16:28:57 -0600

    boxes2.0

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -8,17 +8,27 @@ function App() {
 
   const [squares, setSquares] = React.useState(boxes)
 
+
+  function toggle(id) {
+    setSquares(prevSquares => {
+        return prevSquares.map((square) => {
+            return square.id === id ? {...square, on: !square.on} : square
+        })
+    })
+}
   
 
   const squareElements = squares.map(square => (
 
     
 
-    <Box key={square.id} on={square.on}  />
+    <Box key={square.id} on={square.on} id={square.id} handleClick={toggle}/>
 
     
 
   ) )
+
+  
 
   return (
         <main>
--- boxes/src/Box.js
+++ boxes/src/Box.js
@@ -3,21 +3,19 @@ import boxes from "./boxes";
 
 export default function Box(props){
 
-    const [on, setOn] = React.useState(props.on)
+    // // derived props
+    // const [on, setOn] = React.useState(props.on)
 
     const styles ={
 
-        backgroundColor: on?"#222222":"transparent"
+        backgroundColor: props.on?"#222222":"transparent"
     }
 
-    function toggle(){
-
-        setOn(prevOn => !prevOn)
-    }
+   
 
     return (
 
-        <div className="box" style={styles} onClick={toggle}></div>
+        <div className="box" style={styles} onClick={ ()=>props.handleClick(props.id) }></div>
     )
 
 
